450d661777af04bc9b7b89bc21cb64555c9bb974
MarvinGago <144975718+MarvinGago@users.noreply.github.com>
2025-03-29 16:24:53 -0600

    Add files via upload

---
+++ Scritp/Scirpt de practica3 SQL.sql
@@ -0,0 +1,32 @@
+-- Script de bases de datos
+
+Create database Practica3;
+use Practica3;
+
+-- Crear tabla Autores
+CREATE TABLE Autores (
+    AutorId INT PRIMARY KEY,
+    Nombre VARCHAR(100),
+    Apellido VARCHAR(100),
+    FechaNac DATE
+);
+
+-- Crear tabla Libros
+CREATE TABLE Libros (
+    LibroId INT PRIMARY KEY,
+    Titulo VARCHAR(255),
+    AutorId INT FOREIGN KEY REFERENCES Autores(AutorId),
+    CategoriaId INT,
+    FechaPub DATE,
+    ISBN VARCHAR(20)
+);
+
+CREATE TABLE Categorias (
+    CategoriaId INT PRIMARY KEY IDENTITY(1,1),
+    Nombre VARCHAR(100) NOT NULL,
+    Descripcion VARCHAR(500),
+    FechaCreacion DATETIME DEFAULT GETDATE(),
+    UltimaModificacion DATETIME,
+    EstadoActivo BIT DEFAULT 1,
+    OrdenVisualizacion INT,
+);
